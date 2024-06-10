575f5a880b3198c55807e182216172b6086040bd
Osama Yunus <osamayunus999@gmail.com>
2024-06-10 23:54:02 +0600

    sits

--- App/app.js
+++ App/app.js
@@ -1,10 +1,37 @@
-let availableSits= document.getElementById('available-sits');
 let sitA1Id=document.getElementById('sit-A1');
- document.getElementById('sit-A1').addEventListener('click', function(availableSits,sitA1Id){
+let isOriginalColor = true;
+ document.getElementById('sit-A1').addEventListener('click', function(){
     console.log('clicked');
-   const textGreen= sitA1Id.style.background = "#1DD100";
-   const textWhite=sitA1Id.style.color='white';
-    return (textGreen,textWhite);
+    const selectedSit=document.getElementById('seleted-sit');
+    let availableSits=document.getElementById('available-sits');
+     if (isOriginalColor) {
+      this.style.backgroundColor = '#1DD100';
+      this.style.color = 'white';
+       const intoNum=availableSits.innerText;
+       const intNum=parseInt(intoNum);
+       let sitNum=intNum-1;
+       availableSits.innerText=sitNum; 
+
+       const innerTextNum=selectedSit.innerText;
+       const IntoNum=parseInt(innerTextNum);
+       let selectedSitLeft=IntoNum+1;
+       selectedSit.innerText=selectedSitLeft;
+
+    } 
+  else {
+      this.style.backgroundColor = '';
+      this.style.color = '';
+       const intoNum=availableSits.innerText;
+       const intNum=parseInt(intoNum);
+       let sitNum=intNum+1;
+
+        const innerTextNum=selectedSit.innerText;
+       const IntoNum=parseInt(innerTextNum);
+       let selectedSitLeft=IntoNum-1;
+       selectedSit.innerText=selectedSitLeft;
+       availableSits.innerText=sitNum; 
+        }
+  isOriginalColor = !isOriginalColor;
    })
 
 
