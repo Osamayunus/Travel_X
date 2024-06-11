1e20e2252f33cd20a4c121eff07ee230f3587088
Osama Yunus <osamayunus999@gmail.com>
2024-06-11 17:43:28 +0600

    js-conditional

--- App/app.js
+++ App/app.js
@@ -1,22 +1,40 @@
 let sitA1Id=document.getElementById('sit-A1');
-let isOriginalColor = true;
+let isOriginal = true;
  document.getElementById('sit-A1').addEventListener('click', function(){
     console.log('clicked');
-    const selectedSit=document.getElementById('seleted-sit');
+    const pricePerSit=document.getElementById('sit-price');
+    const sitDis=document.getElementById('sit-part');
+    const sitCode=document.getElementById('Sit-class');
+    const selectedSit=document.getElementById('selected-sit');
     let availableSits=document.getElementById('available-sits');
-     if (isOriginalColor) {
+    let totalPrice=document.getElementById('total-price');
+    let grandPrice=document.getElementById('Grand-price');
+
+     if (isOriginal) {
       this.style.backgroundColor = '#1DD100';
       this.style.color = 'white';
+      const innerTextSit=this.innerText;
+      // adding sit
+      sitCode.innerText=innerTextSit;
+      sitDis.classList.remove('hidden');
+      // total sit left
        const intoNum=availableSits.innerText;
        const intNum=parseInt(intoNum);
        let sitNum=intNum-1;
        availableSits.innerText=sitNum; 
-
+// selected sits
        const innerTextNum=selectedSit.innerText;
        const IntoNum=parseInt(innerTextNum);
        let selectedSitLeft=IntoNum+1;
        selectedSit.innerText=selectedSitLeft;
-
+// total price
+       const intoPricePerSit=pricePerSit.innerText;
+       const intoNumPrice=parseInt(intoPricePerSit);
+       const totalPriceValueNum=parseInt(totalPrice.innerText);
+       let totalPriceAdd=intoNumPrice+totalPriceValueNum;
+       totalPrice.innerText=totalPriceAdd;
+// grand price
+grandPrice.innerText=totalPriceAdd;
     } 
   else {
       this.style.backgroundColor = '';
@@ -24,14 +42,34 @@ let isOriginalColor = true;
        const intoNum=availableSits.innerText;
        const intNum=parseInt(intoNum);
        let sitNum=intNum+1;
+       sitDis.classList.add('hidden');
 
         const innerTextNum=selectedSit.innerText;
        const IntoNum=parseInt(innerTextNum);
        let selectedSitLeft=IntoNum-1;
        selectedSit.innerText=selectedSitLeft;
        availableSits.innerText=sitNum; 
+
+      //  total price
+      const intoPricePerSit=pricePerSit.innerText;
+      const intoNumPrice=parseInt(intoPricePerSit);
+      const totalPriceValueNum=parseInt(totalPrice.innerText);
+      let totalPriceAdd=intoNumPrice-totalPriceValueNum;
+      totalPrice.innerText=totalPriceAdd;
+
+// grand price
+grandPrice.innerText=totalPriceAdd;
+
         }
-  isOriginalColor = !isOriginalColor;
+  isOriginal = !isOriginal; 
    })
 
-
+document.getElementById('apply-discount').addEventListener('click',function(){
+const cuponCode15=document.getElementById('cupon-code-15');
+const cuponCode20=document.getElementById('cupon-code-20');
+const inputText=document.getElementById('input').value
+const cupon15InnerText=cuponCode15.innerText;
+const cupon20InnerText=cuponCode20.innerText;
+ 
+}
+})
